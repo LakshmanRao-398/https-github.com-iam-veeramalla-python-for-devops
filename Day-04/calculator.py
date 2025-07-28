4a5d1784e5ed964a26f6e458ff1fd39ff2ff25e6
LakshmanRao-398 <murari.lakshmanrao398@gmail.com>
2025-07-28 11:37:59 +0530

    Create calculator.py

---
+++ Day-04/calculator.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+print(addition)
+
+substraction = num1 - num2
+print(substraction)
+
+multiplication = num1 * num2
+print(multiplication)
+
+division = num1 / num2
+print(division)
